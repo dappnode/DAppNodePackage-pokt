9e54249ffec99e5c760d1e2c1a557d8c547b3b45
Voss <voss@visnovalabs.io>
2023-07-12 22:56:00 -0700

    update snapshot commands fix indent in config

--- pocket/entrypoint.sh
+++ pocket/entrypoint.sh
@@ -80,8 +80,8 @@ if [ "$NETWORK" == "mainnet" ] && ! $is_update; then
   echo "${INFO} SNAPSHOT Url: ${SNAPSHOT_URL}"
   echo "${INFO} Initializing with SNAPSHOT, it could take several hours..."
   start_downloading_ui
-  mkdir -p /home/app/.pocket/data
-  cd /home/app/.pocket/data
+  mkdir -p /home/app/.pocket/
+  cd /home/app/.pocket/
 
   #Update snapshot to Liquify Compressed Version to save disk space and bandwidth during initial sync
   echo "${INFO} Downloading snapshot file version..."
@@ -92,8 +92,8 @@ if [ "$NETWORK" == "mainnet" ] && ! $is_update; then
   if [[ $SNAPSHOT_URL == *compressed.txt* ]]
   then
     echo "${INFO} Downloading and decompressing the latest compressed snapshot file..."
-    echo "${INFO} wget -c -O - https://pocket-snapshot.liquify.com/files/$latestFile | lz4 -d - | tar -xvf -"
-    wget -c -O - "https://pocket-snapshot.liquify.com/files/$latestFile" | lz4 -d - | tar -xvf -
+    echo "${INFO} wget -c -O - https://pocket-snapshot.liquify.com/files/$latestFile | lz4 -d - | tar -xv -"
+    wget -c -O - "https://pocket-snapshot.liquify.com/files/$latestFile" | lz4 -d - | tar -xv -
     echo "${INFO} Snapshot Downloaded and Decompressed!"
     echo "${INFO} Removing temporary snapshot file metadata..."
     rm latest.txt
@@ -101,8 +101,8 @@ if [ "$NETWORK" == "mainnet" ] && ! $is_update; then
     stop_downloading_ui
   else
     echo "${INFO} Downloading and decompressing the latest uncompressed snapshot file..."
-    echo "${INFO} wget -c -O - https://pocket-snapshot.liquify.com/files/$latestFile | tar -xvf -"
-    wget -c -O - "https://pocket-snapshot.liquify.com/files/$latestFile" | tar -xvf -
+    echo "${INFO} wget -c -O - https://pocket-snapshot.liquify.com/files/$latestFile | tar -xv -"
+    wget -c -O - "https://pocket-snapshot.liquify.com/files/$latestFile" | tar -xv -
     echo "${INFO} Snapshot Downloaded and Decompressed!"
     echo "${INFO} Removing temporary snapshot file metadata..."
     rm latest.txt
